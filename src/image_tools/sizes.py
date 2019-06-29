ef61e13d0a9186810b15190810b9dd5b3e3a8c9b
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-29 11:45:40 -0400

    refactor: Renames package and module.

--- src/image_tools/sizes.py
+++ src/image_tools/sizes.py
@@ -1,13 +1,10 @@
-"""
-https://gist.github.com/sigilioso/2957026
-"""
 try:
     from PIL import Image
 except ImportError:
     import Image
 
 
-def resize_and_crop(path, size, crop_origin='middle'):
+def resize_and_crop(path, size, crop_origin="middle"):
     """
     Resize and crop an image to fit the specified size.
 
@@ -30,36 +27,45 @@ def resize_and_crop(path, size, crop_origin='middle'):
     # Get current and desired ratio for the images
     img_ratio = img.size[0] / float(img.size[1])
     ratio = size[0] / float(size[1])
-    #The image is scaled/cropped vertically or horizontally depending on the ratio
+    # The image is scaled/cropped vertically or horizontally depending on the ratio
     if ratio > img_ratio:
-        img = img.resize((size[0], int(round(size[0] * img.size[1] / img.size[0]))),
-            Image.ANTIALIAS)
+        img = img.resize(
+            (size[0], int(round(size[0] * img.size[1] / img.size[0]))), Image.ANTIALIAS
+        )
         # Crop in the top, middle or bottom
-        if crop_origin == 'top':
+        if crop_origin == "top":
             box = (0, 0, img.size[0], size[1])
-        elif crop_origin == 'middle':
-            box = (0, int(round((img.size[1] - size[1]) / 2)), img.size[0],
-                int(round((img.size[1] + size[1]) / 2)))
-        elif crop_origin == 'bottom':
+        elif crop_origin == "middle":
+            box = (
+                0,
+                int(round((img.size[1] - size[1]) / 2)),
+                img.size[0],
+                int(round((img.size[1] + size[1]) / 2)),
+            )
+        elif crop_origin == "bottom":
             box = (0, img.size[1] - size[1], img.size[0], img.size[1])
-        else :
-            raise ValueError('ERROR: invalid value for crop_origin')
+        else:
+            raise ValueError("ERROR: invalid value for crop_origin")
         img = img.crop(box)
     elif ratio < img_ratio:
-        img = img.resize((int(round(size[1] * img.size[0] / img.size[1])), size[1]),
-            Image.ANTIALIAS)
+        img = img.resize(
+            (int(round(size[1] * img.size[0] / img.size[1])), size[1]), Image.ANTIALIAS
+        )
         # Crop in the top, middle or bottom
-        if crop_origin == 'top':
+        if crop_origin == "top":
             box = (0, 0, size[0], img.size[1])
-        elif crop_origin == 'middle':
-            box = (int(round((img.size[0] - size[0]) / 2)), 0,
-                int(round((img.size[0] + size[0]) / 2)), img.size[1])
-        elif crop_origin == 'bottom':
+        elif crop_origin == "middle":
+            box = (
+                int(round((img.size[0] - size[0]) / 2)),
+                0,
+                int(round((img.size[0] + size[0]) / 2)),
+                img.size[1],
+            )
+        elif crop_origin == "bottom":
             box = (img.size[0] - size[0], 0, img.size[0], img.size[1])
-        else :
-            raise ValueError('ERROR: invalid value for crop_origin')
+        else:
+            raise ValueError("ERROR: invalid value for crop_origin")
         img = img.crop(box)
-    else :
-        img = img.resize((size[0], size[1]),
-            Image.ANTIALIAS)
+    else:
+        img = img.resize((size[0], size[1]), Image.ANTIALIAS)
     return img
